e9b9668182c8603397658446ffcd8bd4a563eef2
Leo Abrantes <leoabrantesmelo@gmail.com>
2022-07-14 19:40:03 -0300

    Project created

---
+++ backend/src/test/java/com/leoabrantes/dsmeta/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoabrantes.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
