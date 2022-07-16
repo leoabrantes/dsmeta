6c920aad7dac5fbfd3cc78132fa0c84391facfd1
Leo Abrantes <leoabrantesmelo@gmail.com>
2022-07-16 19:31:34 -0300

    API test

---
+++ backend/src/main/java/com/leoabrantes/dsmeta/services/SaleService.java
@@ -0,0 +1,22 @@
+package com.leoabrantes.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leoabrantes.dsmeta.entities.Sale;
+import com.leoabrantes.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSales() {
+		return repository.findAll();
+				
+	}
+
+}
